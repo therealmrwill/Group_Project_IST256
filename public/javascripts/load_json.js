c196c72cd70a9a4a33f1fa2c03898ee79e8b1672
therealmrwill <wru5006@psu.edu>
2022-10-20 17:39:19 -0400

    Added Search Function to order page

--- public/javascripts/load_json.js
+++ public/javascripts/load_json.js
@@ -1,12 +1,25 @@
 function updateImage(data){
     for(i=0; i<data.length; i++){
         const imageInfo = data[i];
-        const img = $("<img/>").attr("src", "/images/"+imageInfo.image);
-        const div = $("<div><div>".append(img));
-        $("#images").append(div);
+        const span = $("<p id='prebuiltSandwiches'>").text(imageInfo.title);
+        const img = $("<img/ id='prebuiltSandwiches'>").attr("src", "/images/"+imageInfo.image);
+        const div = $("<div></div>").append(img).append(span);
+        $("#imageBuilder").append(div);
     }
 }
 
 $(document).ready(function (){
-    $.get("data/images.json", updatesImage);
+    $.get("../data/prebuiltSubs.json", updateImage);
 })
+
+
+$(document).ready(function(){
+    $("#searchItem").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#imageBuilder div").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
+
+  
